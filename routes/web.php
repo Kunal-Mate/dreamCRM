1f96c802842be92739b40a8376f67c7349c97db3
Kunal-Mate <kunalm.mate@gmail.com>
2025-06-24 15:35:35 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -10,3 +11,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/index2', function () {
+    return view('index-new');
+});
+
+Route::get('/admin.dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/login', function () {
+    return view('admin.auth.login');
+})->name('admin.auth.login');
+Route::get('/register', function () {
+    return view('admin.auth.register');
+})->name('admin.auth.register');
+
